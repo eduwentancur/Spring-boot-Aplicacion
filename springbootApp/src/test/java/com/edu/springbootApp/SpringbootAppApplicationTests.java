4bd7b9dbfc56ae5c3bbe066cfb7e1e50924faef5
Eduardo Wentancur <eduardo.wentancur123@gmail.com>
2022-04-13 12:49:39 -0300

    creado las entidades Usuario y Roles

---
+++ springbootApp/src/test/java/com/edu/springbootApp/SpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.springbootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

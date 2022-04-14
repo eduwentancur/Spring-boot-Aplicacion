df38b19d7d62c61b6b9b45ef9721771547ba2115
Eduardo Wentancur <eduardo.wentancur123@gmail.com>
2022-04-14 12:36:44 -0300

    arreglado el each en html de usuarios

--- springbootApp/src/main/java/com/edu/springbootApp/controllers/UserController.java
+++ springbootApp/src/main/java/com/edu/springbootApp/controllers/UserController.java
@@ -1,20 +1,36 @@
 package com.edu.springbootApp.controllers;
 
+import com.edu.springbootApp.entity.User;
+import com.edu.springbootApp.repository.RoleRepository;
+import com.edu.springbootApp.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     
+    @Autowired
+    RoleRepository roleRepository;
 
-    @GetMapping("/")
-    public String index(){
-        return "index";
-    }
+    @Autowired
+    UserService userService;
 
+    
     @GetMapping("/userForm")
-    public String userForm(){
-        return "user-form/user-view";
-    }
+	public String getUserForm(Model model) {
+		model.addAttribute("userForm", new User());
+		model.addAttribute("roles",roleRepository.findAll());
+		model.addAttribute("userList", userService.getAllUsers());
+		model.addAttribute("listTab","active");
+		return "user-form/user-view";
+	}
+	
+	@GetMapping({"/","/login"})
+	public String index() {
+		return "index";
+	}
 
 }
--- springbootApp/src/main/java/com/edu/springbootApp/services/UserSeriviceImpl.java
+++ springbootApp/src/main/java/com/edu/springbootApp/services/UserSeriviceImpl.java
@@ -20,4 +20,6 @@ public class UserSeriviceImpl implements UserService {
     
 
     
+
+    
 }
